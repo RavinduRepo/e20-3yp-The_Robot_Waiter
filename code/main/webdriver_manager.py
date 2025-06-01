bd7e37549463dd4abb3ed8ed95ffeac32d4036e9
RaviAlpha <ravindupathirage12345@gmail.com>
2025-06-01 22:52:45 +0530

    finalising script

--- code/main/webdriver_manager.py
+++ code/main/webdriver_manager.py
@@ -16,7 +16,7 @@ def setup_webdriver():
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.addArguments("disable-infobars");
+        chrome_options.addArguments("disable-infobars")
         chrome_options.add_argument("--disable-web-security")
         chrome_options.add_argument("--allow-running-insecure-content")
         chrome_options.add_argument("--disable-extensions")
